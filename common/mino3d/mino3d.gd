035f904665639aac12ee23a7c3a0a1e334400437
CasBogart <casbogart@outlook.com>
2026-07-25 21:11:32 -0400

    mino3d updates
    search animation + imported to actual charbody3d for easier movement and rotation

--- common/mino3d/mino3d.gd
+++ common/mino3d/mino3d.gd
@@ -1,13 +1,22 @@
-class_name Mino3D extends Node3D
+class_name Mino3D extends CharacterBody3D
 
-@onready var state_machine: StateMachine = $StateMachine
-@onready var anim_player: AnimationPlayer = $AnimationPlayer
-
-var velocity = Vector3(0, 0, 0)
+@onready var state_machine: StateMachine = $mino/StateMachine
+@onready var anim_player: AnimationPlayer = $mino/AnimationPlayer
+@onready var nav_agent: NavigationAgent3D = $mino/NavigationAgent3D
 
 func _ready() -> void:
 	state_machine.init(self)
 	SignalBus.player_running.connect(player_follow)
+
+func _process(delta: float) -> void:
+	state_machine.process(delta)
+
+func _physics_process(delta: float) -> void:
+	state_machine.process_physics(delta)
+	move_and_slide()
+
+func _unhandled_input(event: InputEvent) -> void:
+	state_machine.process_input(event)
 
 func player_follow(_pos: Vector3):
 	pass
--- global/test_3d.gd
+++ global/test_3d.gd
@@ -1,12 +1,12 @@
 extends Node3D
 
-var mino3D = preload("res://common/mino3d/mino3d.tscn")
+var mino3D = preload("res://common/mino3d/minotaurScene3D.tscn")
 
 func inst(pos: Vector3, object: Resource, rot = 0):
 	var instance = object.instantiate()
 	# no clue why this is half throwing an error??
-	instance.set_global_position(pos)
-	instance.set_global_rotation(rot)
+	instance.set_global_position.call_deferred(pos)
+	instance.set_global_rotation.call_deferred(rot)
 	add_child(instance)
 
 func _ready():
